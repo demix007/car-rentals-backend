ac7ce1574eafc6280f32bc655905bbcececb6759
demix007 <ayodejiabidemi007@gmail.com>
2023-05-09 10:35:00 +0100

    add actions to car controller

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -1,57 +1,51 @@
-class CarsController < ApplicationController
-    before_action :set_car, only: %i[show update destroy]
+module Api
+  module V1
+    class CarsController < ApplicationController
+      before_action :set_car, only: %i[update show destroy]
+      # GET /cars
+      def index
+        @cars = Car.all
+        render json: CarsRepresenter.new(@cars).as_json
+      end
 
-    # GET /cars
-    def index
-      @cars = Car.all
-  
-      render json: @cars
-    end
-  
-    # GET /cars/1
-    def show
-      render json: @car
-    end
-  
-    # POST /cars
-    def create
-     @car = Car.create(Car_params)
-  
-      if @car.save!
-        render json: { message: 'Car Created Successfully' }, status: :created
-      else
-        render json: @car.errors, status: :unprocessable_entity
+      # POST /car
+      def create
+        @car = Car.create(car_params)
+        if @car.save
+          render json: CarRepresenter.new(@car).as_json, status: :created
+        else
+          render json: @car.errors, status: :unprocessable_entity
+        end
+      end
+
+      # GET /cars/:id
+      def show
+        render json: CarRepresenter.new(@car).as_json
+      end
+
+      # PUT /cars/:id
+      def update
+        @car.update(car_params)
+        head :no_content
+      end
+
+      # DELETE /cars/:id
+      def destroy
+        @car.destroy
+        head :no_content
+      end
+
+      private
+
+      def car_params
+        params.permit(:name, :description, :photo, :price, :model, :user_id )
+      end
+
+      def set_car 
+        @car = Car.find(params[:id])
       end
     end
-  
-    # PATCH/PUT /cars/1
-    def update
-      if@car.update(car_params)
-        render json: @car
-      else
-        render json: @car.errors, status: :unprocessable_entity
-      end
-    end
-  
-    # DELETE /cars/1
-    def destroy
-     @car = Car.find(params[:id])
-      if@car.destroy
-        render json: { message: 'Car deleted successfully' }, status: :no_content
-      else
-        render json: @car.errors, status: :unprocessable_entity
-      end
-    end
-  
-    private
-  
-    # Use callbacks to share common setup or constraints between actions.
-    def set_car
-     @car = Car.find(params[:id])
-    end
-  
-    # Only allow a list of trusted parameters through.
-    def car_params
-      params.require(:car).permit(:name, :description, :photo, :price, :model, :user_id)
-    end
-end
+  end
+ end
+ 
+ 
