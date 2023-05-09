81a3809216e25f0a973103bfd8d6225514f62294
demix007 <ayodejiabidemi007@gmail.com>
2023-05-09 12:46:05 +0100

    Fix Linter errors

--- spec/requests/cars_request_spec.rb
+++ spec/requests/cars_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 RSpec.describe 'Cars', type: :request do
   describe 'GET /api/v1/cars' do
-    let (:user) { create(:user) }
+    let(:user) { create(:user) }
     it 'returns http success' do
       get '/api/v1/cars'
       expect(response).to have_http_status(:success)
@@ -12,17 +12,23 @@ RSpec.describe 'Cars', type: :request do
     end
   end
   describe 'POST /api/v1/cars/:id' do
-    let (:user) { create(:user) }
+    let(:user) { create(:user) }
     it 'creates a car' do
-      post '/api/v1/cars', params: { car: { name: 'My Car', description: 'My Car description', photo: 'My Car photo', price: 100, model: 'My Car model', user: user.id } }
+      post '/api/v1/cars',
+           params: { car: { name: 'My Car', description: 'My Car description', photo: 'My Car photo', price: 100,
+                            model: 'My Car model', user: user.id } }
       expect(json['name']).to eq ["can't be blank"]
     end
     it 'updates a car' do
-      post '/api/v1/cars', params: { car: { name: 'My Car', description: 'My Car description', photo: 'My Car photo', price: 100, model: 'My Car model', user: user.id } }
+      post '/api/v1/cars',
+           params: { car: { name: 'My Car', description: 'My Car description', photo: 'My Car photo', price: 100,
+                            model: 'My Car model', user: user.id } }
       expect(json['name']).to eq ["can't be blank"]
     end
     it 'deletes a car' do
-      post '/api/v1/cars', params: { car: { name: 'My Car', description: 'My Car description', photo: 'My Car photo', price: 100, model: 'My Car model', user: user.id } }
+      post '/api/v1/cars',
+           params: { car: { name: 'My Car', description: 'My Car description', photo: 'My Car photo', price: 100,
+                            model: 'My Car model', user: user.id } }
       expect(json['name']).to eq ["can't be blank"]
     end
   end
