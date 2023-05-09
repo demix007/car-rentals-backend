8469b8ec6f21d9897887649283b3be7f40734f4b
demix007 <ayodejiabidemi007@gmail.com>
2023-05-09 10:36:09 +0100

    Add car resoures to route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,10 @@ Rails.application.routes.draw do
   get 'users/Authentication'
   namespace :api do
     namespace :v1 do
+      resources :users, only: %i[index create destroy]
+      resources :cars, only: %i[index create show update destroy]
       post 'login', to: 'authentication#create'
       post 'register', to: 'users#create'
-      resources :cars, except: :edit
     end
   end
 end
