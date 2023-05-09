8a9cbf6509f7aff51f86a3b048a9d7ed8a38e23b
demix007 <ayodejiabidemi007@gmail.com>
2023-05-09 12:41:23 +0100

    Modify Car Representer

--- app/representers/car_representer.rb
+++ app/representers/car_representer.rb
@@ -2,9 +2,9 @@ class CarRepresenter
     def initialize(car)
       @car = car
     end
-
+    
     def as_json
-      {    
+      {
         id: car.id,
         name: car.name,
         description: car.description,
@@ -15,33 +15,7 @@ class CarRepresenter
         date_added: car.created_at
       }
     end
-
     private
     attr_reader :car
-end
-
-class CarsRepresenter
-    def initialize(cars)
-        @cars = cars
-    end 
-
-    def as_json
-        cars.map do |car|
-            {
-                user_id: car.id,
-                name: car.name,
-                description: car.description,
-                photo: car.photo,
-                price: car.price,
-                model: car.model,
-                user: User.find(car.id).name,
-                date_added: car.created_at
-            }
-        end
-    end
-end
-
-private
-
-attr_reader :cars
-end
+  end
+  
