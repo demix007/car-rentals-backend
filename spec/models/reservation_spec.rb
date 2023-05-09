98dbdd32e569843a22de42a81ef3c537381507f7
demix007 <ayodejiabidemi007@gmail.com>
2023-05-09 14:55:04 +0100

    Write Rspec tests for reservation model

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Reservation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:user)}
+  it { should belong_to(:car)}
+  it { should validate_presence_of(:city)}
+  it { should validate_presence_of(:pick_up)}
+  it { should validate_presence_of(:return_date)}
 end
