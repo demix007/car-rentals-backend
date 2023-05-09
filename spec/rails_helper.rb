f5d83f8ba614f5a0a953dc769c65810f8e0355be
demix007 <ayodejiabidemi007@gmail.com>
2023-05-09 10:36:58 +0100

    enable rails to read from support folder

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -59,7 +59,7 @@ RSpec.configure do |config|
       example.run
     end
   end
-
+  config.include RequestSpecHelper, type: :request
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
