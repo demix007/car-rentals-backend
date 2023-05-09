ee5905d8404e27a458b65a7f90a2d3e59898be2e
demix007 <ayodejiabidemi007@gmail.com>
2023-05-09 20:00:55 +0100

    Add rspec tests for reservation controller

--- spec/requests/reservations_spec.rb
+++ spec/requests/reservations_spec.rb
@@ -2,6 +2,25 @@ require 'rails_helper'
 
 RSpec.describe "Reservations", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "returns http success" do
+      get "/api/v1/reservations"
+      expect(response).to have_http_status(:success)
+    end
+    it "returns a list of reservations" do
+      get "/api/v1/reservations"
+      expect(response).to have_http_status(:success)
+    end
+    it "creates a reservation" do
+      post "/api/v1/reservations", params: { reservation: { user_id: 1, car_id: 1, start_date: '2021-06-01', end_date: '2021-06-02' } }
+      expect(response).to have_http_status(422)
+    end
+    it "updates a reservation" do
+      put "/api/v1/reservations/1", params: { reservation: { user_id: 1, car_id: 1, start_date: '2021-06-01', end_date: '2021-06-02' } }
+      expect(response).to have_http_status(404)
+    end
+    it "deletes a reservation" do
+      delete "/api/v1/reservations/1"
+      expect(response).to have_http_status(404)
+    end
   end
 end
