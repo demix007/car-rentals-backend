c742de1639847f3492f44afca165f3437d31b35c
demix007 <ayodejiabidemi007@gmail.com>
2023-05-09 14:49:17 +0100

    Add validations and associations tns

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :cars, dependent: :destroy
+  has_many :reservations, dependent: :destroy
 
   has_secure_password
   validates :username, presence: true, uniqueness: true, length: { minimum: 3 }
