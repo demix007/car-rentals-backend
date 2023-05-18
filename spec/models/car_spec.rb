652a7e602ddceab62eaf4e4eba2320c0bd8e1ed8
demix007 <ayodejiabidemi007@gmail.com>
2023-05-18 13:50:46 +0100

    modify model spec

--- spec/models/car_spec.rb
+++ spec/models/car_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Car, type: :model do
   it { should belong_to(:user) }
   it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:city)}
   it { should validate_presence_of(:model) }
   it { should validate_presence_of(:price) }
   it { should validate_presence_of(:description) }
