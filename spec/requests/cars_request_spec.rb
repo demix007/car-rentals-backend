b69f77cf4e5e644847d79fbe34d527382fedc122
demix007 <ayodejiabidemi007@gmail.com>
2023-05-18 13:55:33 +0100

    modify car_request_spec.rb

--- spec/requests/cars_request_spec.rb
+++ spec/requests/cars_request_spec.rb
@@ -11,9 +11,10 @@ RSpec.describe 'Cars', type: :request do
       expect(json).to eq []
     end
   end
+  
   describe 'POST /api/v1/cars/:id' do
     let(:user) { create(:user) }
-    
+
     it 'creates a car' do
       post '/api/v1/cars',
            params: { car: { name: 'My Car', description: 'My Car description', photo: 'My Car photo', price: 100,
@@ -27,6 +28,7 @@ RSpec.describe 'Cars', type: :request do
                             model: 'My Car model', user: user.id } }
       expect(json['name']).to eq nil
     end
+
     it 'deletes a car' do
       post '/api/v1/cars',
            params: { car: { name: 'My Car', description: 'My Car description', photo: 'My Car photo', price: 100,
