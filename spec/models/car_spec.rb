422fb26108ab4ad8079b7e02c356e929ac5b917b
demix007 <ayodejiabidemi007@gmail.com>
2023-05-18 13:52:06 +0100

    modify car_spec.rb

--- spec/models/car_spec.rb
+++ spec/models/car_spec.rb
@@ -5,6 +5,6 @@ RSpec.describe Car, type: :model do
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:description) }
   it { should validate_presence_of(:model) }
-  it { should validate_presence_of(:price) }
   it { should validate_presence_of(:photo) }
+  it { should validate_presence_of(:price) }
 end
